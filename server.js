6c407981bf1832852a1aa87d4a8d47a0d3794e02
ishall <ishall@live.com>
2012-12-11 09:24:02 -0500

    basic server

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 
 var http = require("http");
 
-http.create
+function start(){
+    function onRequest(request, response){
+        console.log("Request received");
+        response.writeHead(200,{"Content-Type": "text/plain"});
+        response.write("Hellowwww2");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server started");
+}
+
+exports.start = start;
+
