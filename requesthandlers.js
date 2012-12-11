e6bb4e7645d8c90ac6008f44a64388a510a64835
ishall <ishall@live.com>
2012-12-11 13:13:05 -0500

    update routerhandler

---
+++ requesthandlers.js
@@ -0,0 +1,10 @@
+function start(){
+    console.log("reqhandler start called");
+}
+
+function upload(){
+    console.log("reqhandler upload called")
+}
+
+exports.start = start;
+exports.upload = upload;
--- router.js
+++ router.js
@@ -1,5 +1,12 @@
-function route(pathname){
+function route(handle, pathname){
     console.log("route " + pathname);
+
+    if(typeof handle[pathname] ==='function'){
+        handle[pathname]();
+    }
+    else{
+        console.log("no request found for " + pathname);
+    }
 }
 
 exports.route = route;
